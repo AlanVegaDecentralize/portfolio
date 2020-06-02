3b058b1ecb7e7c8a009dc81e3adc2d9c5ebde7cc
AlanVega <alanvega002@gmail.com>
2020-06-02 11:08:09 -0800

    URL change

--- src/index.js
+++ src/index.js
@@ -26,23 +26,23 @@ import "assets/demo/demo.css";
 import Index from "views/Index.js";
 import LandingPage from "views/examples/LandingPage.js";
 import RegisterPage from "views/examples/RegisterPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
+import VegaPortfolio from "views/examples/ProfilePage.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/components" render={props => <Index {...props} />} />
+      <Route path="/components" render={(props) => <Index {...props} />} />
       <Route
         path="/landing-page"
-        render={props => <LandingPage {...props} />}
+        render={(props) => <LandingPage {...props} />}
       />
       <Route
         path="/register-page"
-        render={props => <RegisterPage {...props} />}
+        render={(props) => <RegisterPage {...props} />}
       />
       <Route
-        path="/profile-page"
-        render={props => <ProfilePage {...props} />}
+        path="/VegaPortfolio"
+        render={(props) => <VegaPortfolio {...props} />}
       />
       <Redirect from="/" to="/components" />
     </Switch>
